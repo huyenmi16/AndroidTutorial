26e67e333b6f9f12ce3df85583e589a841c63715
huyenmi16 <lnhuyen160902@gmail.com>
2024-07-02 14:34:21 +0700

    back navigation

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -19,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.btn)
+        button.setOnClickListener {
+            val intent = Intent(this,DetailActivity::class.java)
+            startActivity(intent)
+        }
 
-        // Add CustomFragment to the activity
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, ExampleFragment())
-            .commit()
+
     }
 }
